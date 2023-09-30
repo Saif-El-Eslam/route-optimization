2c08e6f6f396e0939841dba3e25a9ac2982ddea4
saifeleslam <saif82820@gmail.com>
2023-09-30 13:57:50 +0300

    added redux store

--- Website/frontend/src/ReduxStore/store.js
+++ Website/frontend/src/ReduxStore/store.js
@@ -1,11 +1,5 @@
 import { createStore } from "redux";
 import rideReducer from "./reducer";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "main-root",
-  storage,
-};
 
 const rideStore = createStore(rideReducer);
 
